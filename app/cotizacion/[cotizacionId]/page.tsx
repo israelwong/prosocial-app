204b00439de8f57ce59f01254e7fa273e28c21c7
Israel Wong <israelwong@iMac-de-Israel.local>
2025-01-03 18:55:03 -0600

    font bebas

--- app/cotizacion/[cotizacionId]/page.tsx
+++ app/cotizacion/[cotizacionId]/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Metadata } from 'next'
 import Cotizacion from '../components/Cotizacion'
-import "../../globals.css";
+import "../globals.css";
 
 export const metadata: Metadata = {
     title: 'Cotización personalizada'
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,38 +1,38 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
 			animation: {
 				'infinite-scroll': 'infinite-scroll 100s linear infinite',
 				rotate: 'rotate 10s linear infinite',
 			},
 			keyframes: {
-			'infinite-scroll': {
-				from: { transform: 'translateX(0)' },
-				to: { transform: 'translateX(-100%)' },
+				'infinite-scroll': {
+					from: { transform: 'translateX(0)' },
+					to: { transform: 'translateX(-100%)' },
+				},
+				rotate: {
+					'0%': { transform: 'rotate(0deg) scale(10)' },
+					'100%': { transform: 'rotate(-360deg) scale(10)' },
+				},
 			},
-			rotate: {
-				'0%': { transform: 'rotate(0deg) scale(10)' },
-				'100%': { transform: 'rotate(-360deg) scale(10)' },
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
 			},
-			},
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      fontFamily: {
+			fontFamily: {
 				"Bebas-Neue": ['Bebas Neue', "sans-serif"],
+				"roboto": ['roboto', "sans-serif"],
 				'Smooch': ['"Smooch"', "cursive"],
 			},
-    },
-  },
-  plugins: [],
+		},
+	},
+	plugins: [],
 };
 export default config;
