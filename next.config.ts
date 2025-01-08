5737a96256ab30ac293f80f9aa012064b202d37e
Israel Wong <israelwong@iMac-de-Israel.local>
2025-01-07 19:23:20 -0600

    anon

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
+// filepath: /Users/israelwong/Documents/Desarrollo/prosocial-app/next.config.js
 const nextConfig = {
   images: {
-    domains:
-      ['bgtapcutchryzhzooony.supabase.co'],
+    domains: ['bgtapcutchryzhzooony.supabase.co'],
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
