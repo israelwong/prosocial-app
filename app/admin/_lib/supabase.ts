3a7dca65d63ca97750532c331b3763da847c8889
Israel Wong <israelwong@iMac-de-Israel.local>
2025-01-13 16:34:38 -0600

    uddate

--- app/admin/_lib/supabase.ts
+++ app/admin/_lib/supabase.ts
@@ -1,24 +1,10 @@
-// import { createClient } from '@supabase/supabase-js';
-
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-// const supabaseAdmin = process.env.SUPABASE_SERVICE_ROLE_KEY;
-
-// if (!supabaseUrl || !supabaseAnonKey || !supabaseAdmin) {
-//     throw new Error('Missing Supabase URL, Anon Key, or Admin Key');
-// }
-
-// export const supabase = createClient(supabaseUrl, supabaseAdmin);
-
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAdminKey = process.env.SUPABASE_SERVICE_ROLE_KEY as string;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseAdminKey) {
-    throw new Error('Missing Supabase URL or Admin Key');
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Missing Supabase URL or Anon Key');
 }
 
-const supabase = createClient(supabaseUrl, supabaseAdminKey);
-
-export default supabase;
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
